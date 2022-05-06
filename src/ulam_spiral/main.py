b7a405f372e51bcd48d4f140a57c2b73a28a971b
botamochi6277 <botamochi6277@icloud.com>
2022-05-06 16:21:28 +0900

    add argparse

--- src/ulam_spiral/main.py
+++ src/ulam_spiral/main.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.font_manager as fm
 from typing import List, Tuple
+import argparse
 
 
 def prime_factorize(n: int) -> List[int]:
@@ -41,8 +42,12 @@ def draw(num_edges: int = 20,
          no_color: str = '#cccccc',
          prime_no_color: str = '#4A00E0',
          marker_color: str = '#ffffff',
-         marker_prime_color: str = '#8E2DE2',
-         bg_color: str = '#f7f7f7'):
+         prime_marker_color: str = '#8E2DE2',
+         markersize: float = 32,
+         fontsize: float = 16,
+         bg_color: str = '#f7f7f7',
+         no_window: bool = False,
+         output_name: str = ''):
     rot = np.matrix([[np.cos(np.pi/2), -np.sin(np.pi/2)],
                      [np.sin(np.pi/2), np.cos(np.pi/2)]]
                     )
@@ -52,7 +57,7 @@ def draw(num_edges: int = 20,
     fig.patch.set_facecolor(bg_color)
     fig.patch.set_alpha(1.0)
 
-    n = 0
+    n = 1
     xy = np.array([0, 0])
     v = np.matrix([[1], [0]])
     for edge_length in edge_lengths:
@@ -63,10 +68,11 @@ def draw(num_edges: int = 20,
                 p = prime_factorize(n)
                 if len(p) == 1:
                     no_c = prime_no_color
-                    m_c = marker_prime_color
-            ax.plot(xy[0], xy[1], 'o', color=m_c, markersize=16)  # background
+                    m_c = prime_marker_color
+            ax.plot(xy[0], xy[1], 'o', color=m_c,
+                    markersize=markersize)  # background
             ax.text(xy[0], xy[1], f'{n}', color=no_c,
-                    va='center', ha='center')
+                    va='center', ha='center', fontsize=fontsize)
             xy += np.ravel(v).astype(np.int32)
             n += 1
             if i == (edge_length-1):
@@ -74,8 +80,28 @@ def draw(num_edges: int = 20,
 
     ax.set_aspect('equal')
     ax.axis('off')
-    plt.show()
+    if not no_window:
+        plt.show()
+
+    if len(output_name) > 0:
+        plt.savefig(output_name)
 
 
 if __name__ == '__main__':
-    draw()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--num_edges', type=int, default=20)
+    parser.add_argument('-s', '--figsize', type=int,
+                        nargs=2, default=(800, 600))
+    parser.add_argument('--no_color', default='#cccccc')
+    parser.add_argument('--prime_no_color', default='#4A00E0')
+    parser.add_argument('--marker_color', default='#ffffff')
+    parser.add_argument('--prime_marker_color', default='#8E2DE2')
+    parser.add_argument('--markersize', type=int, default=32)
+    parser.add_argument('--fontsize', type=int, default=16)
+    parser.add_argument('--bg_color', default='#f7f7f7')
+    parser.add_argument('--no_window', action='store_true')
+    parser.add_argument('-o', '--output_name', default='')
+
+    args = parser.parse_args()
+    params = vars(args).copy()
+    draw(**params)
