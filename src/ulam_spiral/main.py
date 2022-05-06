123bc84dc84f51dc0c738bc6ad383ddefe0efae3
botamochi6277 <botamochi6277@icloud.com>
2022-05-06 15:06:09 +0900

    add variables

--- src/ulam_spiral/main.py
+++ src/ulam_spiral/main.py
@@ -36,35 +36,43 @@ def sq_edge_length(i: int) -> int:
     return int(edge)
 
 
-def draw():
+def draw(num_edges: int = 20,
+         no_color: str = '#cccccc',
+         prime_no_color: str = '#4A00E0',
+         marker_color: str = '#ffffff',
+         marker_prime_color: str = '#8E2DE2',
+         bg_color: str = '#f7f7f7'):
     rot = np.matrix([[np.cos(np.pi/2), -np.sin(np.pi/2)],
                      [np.sin(np.pi/2), np.cos(np.pi/2)]]
                     )
-    edge_lengths = [sq_edge_length(i) for i in range(20)]
+    edge_lengths = [sq_edge_length(i) for i in range(num_edges)]
 
     fig, ax = plt.subplots()
+    fig.patch.set_facecolor(bg_color)
+    fig.patch.set_alpha(1.0)
 
     n = 0
     xy = np.array([0, 0])
     v = np.matrix([[1], [0]])
     for edge_length in edge_lengths:
         for i in range(edge_length):
-            # print(n)
-            c = '#cccccc'
+            no_c = no_color
+            m_c = marker_color
             if n >= 2:
                 p = prime_factorize(n)
                 if len(p) == 1:
-                    # print(n)
-                    c = '#cc0000'
-            ax.plot(xy[0], xy[1], 'o', color='white')
-            ax.text(xy[0], xy[1], f'{n}', color=c, va='center', ha='center')
+                    no_c = prime_no_color
+                    m_c = marker_prime_color
+            ax.plot(xy[0], xy[1], 'o', color=m_c, markersize=16)  # background
+            ax.text(xy[0], xy[1], f'{n}', color=no_c,
+                    va='center', ha='center')
             xy += np.ravel(v).astype(np.int32)
             n += 1
             if i == (edge_length-1):
                 v = rot*v
 
     ax.set_aspect('equal')
-
+    ax.axis('off')
     plt.show()
 
 
